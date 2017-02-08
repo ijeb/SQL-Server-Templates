70b3349e0cfbe218d634878950c7fc9e3c8e82e5
ijeb <ijeb_reitsma@outlook.com>
2017-02-08 12:18:15 +0100

    Signed-off-by: ijeb <ijeb_reitsma@outlook.com>

---
+++ .master/dbo.sp_helpjobs.sql
@@ -0,0 +1,106 @@
+﻿CREATE PROCEDURE [dbo].[sp_helpjobs]
+	@Category varchar(100) = NULL,
+	@Name varchar(100) = NULL,
+	@OnlyEnabled bit = 1,
+	@ShowHistory bit = 0,
+	@ShowSteps bit = 0,
+	@OnlyRunning bit = 0,
+	@MinRuntimeMin tinyint = NULL
+AS
+SET NOCOUNT ON;
+SELECT @@servername as instancename
+	,j.job_id 
+	,j.name
+	,CAST(ISNULL(j.[enabled],0) AS bit) job_enabled
+	,CAST(ISNULL(s.[enabled],0) AS bit) schedule_enabled
+	,c.name category
+	,j.description
+	,ISNULL(CAST(s.active_start_time / 10000 AS VARCHAR(10)) + ':' + RIGHT('00' + CAST(s.active_start_time % 10000 / 100 AS VARCHAR(10)), 2),'') AS active_start_time
+	,ISNULL(js.next_run_date,0) AS next_run_date
+	,ISNULL(js.next_run_time,'') AS next_run_time
+	,ISNULL(s.name, '') AS schedule_name
+	,ISNULL(dbo.udf_schedule_description(s.freq_type, 
+		s.freq_interval,  
+		s.freq_subday_type, 
+		s.freq_subday_interval, 
+		s.freq_relative_interval,  
+		s.freq_recurrence_factor, 
+		s.active_start_date, 
+		s.active_end_date,  
+		s.active_start_time, 
+		s.active_end_time),'No schedule') AS schedule_desc
+	,Status = CASE WHEN (start_execution_date is not null AND stop_execution_date is null) THEN 'RUNNING' ELSE 'idle' END
+	,CASE h.run_status
+		WHEN 0 THEN 'Failed'
+		WHEN 1 THEN 'Succeeded'
+		WHEN 2 THEN 'Retry'
+		WHEN 3 THEN 'Cancelled'
+		WHEN 4 THEN 'In Progress'
+		ELSE ''
+     END AS execution_status
+	, ISNULL(CAST(STR(h.run_date, 8, 0) AS DATETIME)
+        + CAST(STUFF(STUFF(RIGHT('000000' + CAST (h.run_time AS VARCHAR(6)), 6),
+                           5, 0, ':'), 3, 0, ':') AS DATETIME), '19000101') AS start_datetime
+	, ISNULL(DATEADD(SECOND,
+                ( ( h.run_duration / 1000000 ) * 86400 )
+                + ( ( ( h.run_duration - ( ( h.run_duration / 1000000 )
+                                           * 1000000 ) ) / 10000 ) * 3600 )
+                + ( ( ( h.run_duration - ( ( h.run_duration / 10000 ) * 10000 ) )
+                      / 100 ) * 60 ) + ( h.run_duration - ( h.run_duration
+                                                            / 100 ) * 100 ),
+                CAST(STR(h.run_date, 8, 0) AS DATETIME)
+                + CAST(STUFF(STUFF(RIGHT('000000'
+                                         + CAST (h.run_time AS VARCHAR(6)), 6),
+                                   5, 0, ':'), 3, 0, ':') AS DATETIME)), '19000101') AS end_datetime
+	, ISNULL(STUFF(STUFF(REPLACE(STR(h.run_duration, 6, 0), ' ', '0'), 3, 0, ':'), 6,
+              0, ':'), '') AS duration_formatted
+	, ISNULL(( ( ( h.run_duration / 1000000 ) * 86400 ) + ( ( ( h.run_duration
+                                                         - ( ( h.run_duration
+                                                              / 1000000 )
+                                                             * 1000000 ) )
+                                                       / 10000 ) * 3600 )
+        + ( ( ( h.run_duration - ( ( h.run_duration / 10000 ) * 10000 ) )
+              / 100 ) * 60 ) + ( h.run_duration - ( h.run_duration / 100 )
+                                 * 100 ) ) / 60, 0) AS duration_min
+	,j.date_created
+	,j.date_modified
+	,j.version_number
+	,jp.name AS [job_owner]
+	,ISNULL(sp.name, '') AS [schedule_owner]
+	,st.step_id
+	,st.step_name
+	,st.subsystem
+	,st.database_name
+	,LEFT(st.command, 1000) command
+	,st.output_file_name
+FROM msdb.dbo.sysjobs j
+LEFT OUTER JOIN msdb.dbo.syscategories c ON c.category_id = j.category_id
+LEFT OUTER JOIN msdb.dbo.sysjobschedules js ON js.job_id = j.job_id
+LEFT OUTER JOIN msdb.dbo.sysschedules s ON s.schedule_id = js.schedule_id
+LEFT OUTER JOIN (SELECT job_id,step_id,step_name,subsystem,database_name,command,output_file_name FROM msdb.dbo.sysjobsteps
+				 UNION ALL
+				 SELECT job_id,0,NULL,NULL,NULL,NULL,NULL
+				 FROM msdb.dbo.sysjobsteps
+				 GROUP BY job_id
+				) st ON st.job_id = j.job_id
+LEFT OUTER JOIN (	SELECT job_id, step_id, max(instance_id) max_instance_id
+					FROM msdb.dbo.sysjobhistory					
+					GROUP BY job_id, step_id ) hmax ON hmax.job_id = j.job_id AND hmax.step_id = st.step_id
+LEFT OUTER JOIN msdb.dbo.sysjobhistory h ON h.job_id = j.job_id AND h.step_id = st.step_id AND (@ShowHistory = 1 OR h.instance_id = hmax.max_instance_id)
+CROSS JOIN (SELECT TOP 1 * FROM msdb.dbo.syssessions ORDER BY session_id DESC) se
+LEFT OUTER JOIN msdb.dbo.sysjobactivity ja ON ja.job_id = j.job_id AND ja.session_id = se.session_id
+LEFT OUTER JOIN sys.server_principals jp ON jp.sid = j.owner_sid
+LEFT OUTER JOIN sys.server_principals sp ON sp.sid = s.owner_sid
+WHERE (c.name = @Category OR @Category IS NULL)
+AND (j.name = @Name OR @Name IS NULL)
+AND (@OnlyEnabled = 0 OR j.[enabled] = 1)
+AND (@ShowSteps = 1 OR st.step_id = 0)
+AND (@OnlyRunning = 0 OR (start_execution_date is not null AND stop_execution_date is null))
+AND ( ( ( ( h.run_duration / 1000000 ) * 86400 ) + ( ( ( h.run_duration
+                                                         - ( ( h.run_duration
+                                                              / 1000000 )
+                                                             * 1000000 ) )
+                                                       / 10000 ) * 3600 )
+        + ( ( ( h.run_duration - ( ( h.run_duration / 10000 ) * 10000 ) )
+              / 100 ) * 60 ) + ( h.run_duration - ( h.run_duration / 100 )
+                                 * 100 ) ) / 60 > @MinRuntimeMin OR @MinRuntimeMin IS NULL)
---
+++ .master/dbo.sp_spaceused2.sql
@@ -0,0 +1,68 @@
+﻿
+CREATE PROCEDURE [dbo].[sp_spaceused2]
+AS
+SET NOCOUNT ON
+DECLARE @allocation_table table
+(
+      dbname sysname,
+      reservedpages bigint,
+      usedpages bigint,
+      pages bigint
+)
+INSERT INTO @allocation_table
+EXEC sp_MSforeachdb N'IF EXISTS
+(
+     SELECT 1 FROM SYS.DATABASES WHERE name = ''?'' AND  NAME NOT IN(''master'',''msdb'',''model'',''tempdb'') and STATE=0
+    --customize to monitor specific databases
+     --SELECT 1 FROM SYS.DATABASES WHERE name = ''?'' AND  NAME IN(''EMPLOYEE'') and STATE=0 
+)
+BEGIN
+     SELECT
+        ''?'',
+            SUM(a.total_pages) as reservedpages,
+            SUM(a.used_pages) as usedpages,
+            SUM(
+                CASE
+                    -- XML-Index and FT-Index internal tables are not considered "data", but is part of "index_size"
+                    When it.internal_type IN (202,204,211,212,213,214,215,216) Then 0
+                    When a.type <> 1 Then a.used_pages
+                    When p.index_id < 2 Then a.data_pages
+                    Else 0
+                END
+            ) as pages
+        from ?.sys.partitions p join ?.sys.allocation_units a on p.partition_id = a.container_id
+        left join ?.sys.internal_tables it on p.object_id = it.object_id
+END';
+SELECT
+		@@SERVERNAME instancename,
+        -- from first result set of 'exec sp_spacedused'
+        db_name(sf.database_id) as [database_name]
+        ,convert(dec (15,2), (convert (dec (15,2),sf.dbsize) + convert (dec (15,2),sf.logsize)) * 8192 / 1048576) as [database_size_mb]
+        ,convert(dec (15,2), (case when sf.dbsize >= pages.reservedpages then
+            (convert (dec (15,2),sf.dbsize) - convert (dec (15,2),pages.reservedpages))
+            * 8192 / 1048576 else 0 end)) as [unallocated space_mb]
+        -- from second result set of 'exec sp_spacedused'
+        ,convert(dec(15,0), (pages.reservedpages * 8192 / 1048576.)) as [reserved_mb]
+        ,convert(dec(15,0), (pages.pages * 8192 / 1048576.)) as data_mb
+        ,convert(dec(15,0), ((pages.usedpages - pages.pages) * 8192 / 1048576.)) as index_size_mb
+        ,convert(dec(15,0), ((pages.reservedpages - pages.usedpages) * 8192 / 1048576.)) as unused_mb
+        -- additional columns data and Log Size
+        ,convert(dec(15,2), (convert (dec (15,2),sf.dbsize)) * 8192 / 1048576)  as dbsize_mb
+        ,convert(dec(15,2), (convert (dec (15,2),sf.logsize)) * 8192 / 1048576)  as logsize_mb
+    FROM (
+        select
+            database_id,
+            sum(convert(bigint,case when type = 0 then size else 0 end)) as dbsize,
+            sum(convert(bigint,case when type <> 0 then size else 0 end)) as logsize
+        from sys.master_files
+        group by database_id
+    ) sf,
+    (
+    SELECT
+            dbname,
+            reservedpages,
+            usedpages,
+            pages
+            FROM @ALLOCATION_TABLE
+     ) pages
+  WHERE DB_NAME(sf.database_id)=pages.dbname
