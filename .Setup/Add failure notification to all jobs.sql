c0603752e82748892cd59a61dc255f6ddf883cbc
ijeb <ijeb_reitsma@outlook.com>
2017-02-08 12:18:05 +0100

    Signed-off-by: ijeb <ijeb_reitsma@outlook.com>

--- .Setup/Add failure notification to all jobs.sql
+++ .Setup/Add failure notification to all jobs.sql
@@ -1,11 +1,11 @@
---To update all jobs that do not have email notification enabled I run this:
+--To update all job notifications
 
 DECLARE @jobid AS NVARCHAR(MAX)
 
 DECLARE jobstep_cursor CURSOR FOR
  SELECT job_id 
  FROM msdb.dbo.sysjobs
- WHERE enabled = 1 AND notify_level_email = 0
+ --WHERE enabled = 1 AND notify_level_email = 0
 
 OPEN jobstep_cursor;
 
@@ -16,10 +16,8 @@ BEGIN
 
 EXEC msdb.dbo.sp_update_job @job_id = @jobid, 
  @notify_level_email=2, 
- @notify_level_netsend=2, 
- @notify_level_page=2, 
  @notify_email_operator_name=N'DBA' -- ! <-- change if needed
-
+ @notify_level_eventlog=2,
 
 FETCH NEXT FROM jobstep_cursor INTO @jobid;
 
